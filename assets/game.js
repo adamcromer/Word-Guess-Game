2ee39e6dc9db75aec791794e1373430d785ae715
adamjorycromer <adamjorycromer@gmail.com>
2018-11-07 16:13:42 -0700

    looking good

--- assets/game.js
+++ assets/game.js
@@ -1,58 +1,84 @@
-/* PsuedoCode
-An array for all of the words the user is going to guess
-key functions if user guesses which letter
-Way to display _ and the correct amount for how many letters in the word
-Something to confirm if you click the right letter that it appears in stead of the _
-Score and how to display x amount of guesses left
-What letters you've already guessed
-Game over or You guess x correctly
+let wordsToGuess = ['bear', 'turkey', 'donkey', 'tarantula', 'scorption', 'pigeon', 'mongoose', 'seagull', 'hawk', 'eagle', 'elephant', 'giraffe', 'tiger', 'lion', 'pangolin', 'penguin', 'zebra', 'flamingo', 'gorilla', 'wolf', 'fox', 'shark', 'octopus', 'monkey', 'lobster', 'hyena', 'moose', 'horse', 'goat', 'llama', 'alpaca', 'cheetah', 'pirahna', 'baracuda', 'manowar', 'capybara', 'emu'];
+        let guessProgress = [];
+        let lettersChosen = [];
+        let lives = 10;
+        let alphabet = "abcdefghijklmnopqrstuvwxyz";
+        let mainBody = document.getElementById("mainBody");
 
-*/
+        // This picks a random index from wordsToGuess and assigns it to currentChoice.
+        let currentChoice = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length + 1) + 1];
+
+        // This assigns the proper number of _ for the currentChoice.
+        for (i = 0; i < currentChoice.length; i++) {
+            guessProgress.push('_');
+        }
+
+        // This logs the currentChoice for testing purposes.
+        console.log(currentChoice);
 
 
-// let wordsToGuess = ['elephant', 'giraffe', 'tiger', 'lion', 'pangolin', 'penguin', 'zebra', 'flamingo', 'gorilla', 'wolf', 'fox'];
-// let guessProgress = [];
-// let lettersChosen = [];
+        document.onkeyup = function (event) {
+            let chosenLetter = event.key.toLowerCase();
 
-// // This picks a random index from wordsToGuess and assigns it to currentChoice.
-// let currentChoice = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)];
+            // Makes sure the button pressed is a letter of the alphabet.
+            if (alphabet.indexOf(chosenLetter) != -1) {
 
-// // This assigns the proper number of _ for the currentChoice.
-// for (i = 0; i < currentChoice.length; i++) {
-//     guessProgress.push('_');
-// }
+                //Finds if the chosenletter is in currentChoice.
+                if (currentChoice.indexOf(chosenLetter) != -1) {
 
-// // This logs the currentChoice for testing purposes.
-// console.log(currentChoice);
+                    //Loops on all characters in currentChoice. Just in case there's multiple.
+                    for (let i = 0; i < currentChoice.length; i++) {
 
-// let chosenLetter = prompt("Enter a letter");
+                        //If it does occur in current choice then set then show it on guessProgres.
+                        if (currentChoice[i] === chosenLetter) {
 
-// // console.log(chosenLetter);
+                            guessProgress[i] = currentChoice[i];
 
-// // document.onkeyup = function(event) {
-// //     let chosenLetter = String.fromCharCode(event.keyCode).toLowerCase();
-// //     console.log(chosenLetter);
+                        }
+                    }
+                }
 
-//     //Finds if the chosenletter is in currentChoice.
-//     if (currentChoice.indexOf(chosenLetter) != -1) {
+                else {
 
-//         //Loops on all characters in currentChoice. Just in case there's multiple.
-//         for (let i = 0; i < currentChoice.length - 1; i++) {
+                    if (lettersChosen.indexOf(chosenLetter) === -1) {
+                        lettersChosen.push(chosenLetter);
+                        lives = lives - 1;
+                    }
 
-//             //If it does occur in current choice then set then show it on guessProgres.
-//             if (currentChoice[i] === chosenLetter) {
-//                 guessProgress[i] = currentChoice[i];
-//             }
-//         }
-//     }
+                }
 
-//     else {
-//         lettersChosen.push(chosenLetter);
-//     }
+                // This assigns the H1 element to show on screen and replaces commas with spaces instead.
+                document.getElementById("blankToGuess").innerHTML = guessProgress.join(' ');
+                document.getElementById("lettersChosen").innerHTML = lettersChosen.join(' ');
+                document.getElementById("livesRemaining").innerHTML = lives;
 
-// // }
+                if (currentChoice === guessProgress.join('')) {
+                    let winDiv = document.createElement("h2");
+                    let tryAgain = document.createElement("a");
+                    tryAgain.setAttribute("id", "AgainButton");
+                    winDiv.textContent = ("You win!");
+                    tryAgain.textContent = ("Try again?");
+                    tryAgain.href = ("/Word-Guess-Game/index.html")
+                    mainBody.textContent = ("");
+                    mainBody.appendChild(winDiv);
+                    mainBody.appendChild(tryAgain);
+                }
 
-// // This assigns the H1 element to show on screen and replaces commas with spaces instead.
-// document.getElementById("blankToGuess").innerHTML = guessProgress.join(' ');
-// document.getElementById("lettersChosen").innerHTML = lettersChosen.join(' ');
+                if (lives === 0) {
 
+                    let loseDiv = document.createElement("h2");
+                    let tryAgain = document.createElement("a");
+                    tryAgain.setAttribute("id", "AgainButton");
+                    loseDiv.textContent = ("You lose! The correct answer was " + currentChoice + ".");
+                    tryAgain.textContent = ("Try again?");
+                    tryAgain.href = ("/Word-Guess-Game/index.html")
+                    mainBody.textContent = ("");
+                    mainBody.appendChild(loseDiv);
+                    mainBody.appendChild(tryAgain);
+                }
+            }
+        }
+        // let container = document.getElementById("container").innerHTML
+        document.getElementById("blankToGuess").innerHTML = guessProgress.join(' ');
+        document.getElementById("lettersChosen").innerHTML = lettersChosen.join(' ');
+        document.getElementById("livesRemaining").innerHTML = lives;
